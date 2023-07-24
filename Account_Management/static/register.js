4625bb899a465c77925041974fcabfe10cdd8a1d
Spriteson <duan_hui@hotmail.com>
2023-07-24 21:10:40 +1000

    js cannot read form's objects

--- Account_Management/static/register.js
+++ Account_Management/static/register.js
@@ -1,16 +1,13 @@
 function handleLabel(field, labelId) {
-        const inputField = document.getElementById(field);
-        const label = document.getElementById(labelId);
+    const inputField = document.getElementById(field);
+    const label = document.getElementById(labelId);
 
-        inputField.addEventListener("input", function() {
-            if (inputField.value.trim() !== "") {
-                label.style.display = "none";
-            } else {
-                label.style.display = "inline-block";
-            }
-        });
-    }
-handleLabel("{{ form.username.id_for_label }}", "username_label");
-handleLabel("{{ form.email.id_for_label }}", "email_label");
-handleLabel("{{ form.password1.id_for_label }}", "password1_label");
-handleLabel("{{ form.password2.id_for_label }}", "password2_label");
+    inputField.addEventListener("input", function() {
+        if (inputField.value.trim() !== "") {
+            label.style.display = "none";
+        } else {
+            label.style.display = "inline-block";
+        }
+    });
+}
+    
