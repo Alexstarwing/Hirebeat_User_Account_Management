c88eaf87ede6eca012812cb4d628bb19d914778c
lavywei666 <yifan.wei@vanderbilt.edu>
2023-07-29 22:06:09 +0800

    manage user done|autocomplete company not done

--- Account_Management/forms.py
+++ Account_Management/forms.py
@@ -4,3 +4,6 @@ from django import forms
 class InviteForm(forms.Form):
     name = forms.CharField(max_length=255)
     email = forms.EmailField(required=True, label='Email')
+
+class OrganizationForm(forms.Form): 
+    organization = forms.CharField(max_length=200, label='Organization Name')
