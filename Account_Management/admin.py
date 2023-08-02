ad6eb1e734e6c3709ab8e45cc923976e3c959fe1
Spriteson <duan_hui@hotmail.com>
2023-08-02 23:25:54 +0800

    Roles, TeamInvitation, CustomUsers shown in admin

--- Account_Management/admin.py
+++ Account_Management/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Account
+from .models import Account, Role, TeamInvitation
 
 # Register your models here.
 admin.site.register(Account)
+admin.site.register(Role)
+admin.site.register(TeamInvitation)
