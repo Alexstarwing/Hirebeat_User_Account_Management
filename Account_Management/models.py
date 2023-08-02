89321abcb6b662c209c001a38fdfe53dc3171729
Spriteson <duan_hui@hotmail.com>
2023-08-02 23:07:25 +0800

    added email, industry

--- Account_Management/models.py
+++ Account_Management/models.py
@@ -5,19 +5,39 @@ from User_Management.models import Profile
 
 
 class Account(models.Model):
+    ACCOUNT_STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('expired', 'Expired'),
+        ('fraud', 'Fraud'),
+    ]
+
+    COMPANY_SIZE_RANGE_CHOICES = [
+        ('1-50', '1-50'),
+        ('51-200', '51-200'),
+        ('201-500', '201-500'),
+        ('501-1000', '501-1000'),
+        ('1001+', '1001+'),
+    ]
+    
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     office = models.CharField(max_length=200)
     department = models.CharField(max_length=200)
     organization = models.CharField(max_length=200)
     profile_logo = models.ImageField(null=True, blank=True)
     company_name = models.CharField(max_length=255, null=True, blank=True)
+    company_email = models.EmailField(max_length=255, null=True, blank=True)
+    # account_status = models.CharField(max_length=255, choices=ACCOUNT_STATUS_CHOICES)
     company_summary = models.TextField(null=True, blank=True)
+    company_industry = models.CharField(max_length=255, null=True, blank=True)
+    #company_size_range = models.CharField(max_length=255, null=True, blank=True, choices=COMPANY_SIZE_RANGE_CHOICES)
     company_linkedin = models.URLField(max_length=255, null=True, blank=True)
     company_facebook = models.URLField(max_length=255, null=True, blank=True)
     company_twitter = models.URLField(max_length=255, null=True, blank=True)
     company_video_url = models.URLField(max_length=255, null=True, blank=True)
     viewed_employer_welcome = models.TextField(null=True, blank=True)
     viewed_employer_tutorial = models.TextField(null=True, blank=True)
+    #created_at = models.DateTimeField(auto_now_add=True)
+    #updated_at = models.DateTimeField(auto_now=True)
 
 
     
@@ -27,3 +47,24 @@ class Account(models.Model):
     #     first_user = users[0].name if users else 'No users'
     #     return f'{first_user}, Organization: {self.organization}, Office: {self.office}, Department: {self.department}'
     
+class Role(models.Model):
+    ROLE_TYPE_CHOICES = [
+        ('admin', 'Admin'),
+        ('hiring_manager', 'Hiring_Manager'),
+        ('recruiter', 'Recruiter'),
+        ('coordinator', 'Coordinator'),
+        ('sourcer', 'Sourcer'),
+        ('external_reviewer', 'External_Reviewer'),
+        ('sub_reviewer', 'Sub_Reviewer'),
+    ]
+
+    role_name = models.CharField(max_length=255, unique=True)
+    role_type = models.CharField(max_length=255, choices=ROLE_TYPE_CHOICES)
+    role_description = models.TextField(blank=True, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['role_type']),
+        ]
