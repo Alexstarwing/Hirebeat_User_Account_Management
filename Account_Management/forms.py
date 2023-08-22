337d509a2eb4f0706b3eb67571db3a231d1581e5
Alexstarwing <441698225@qq.com>
2023-08-22 16:44:33 -0400

    style add user page

--- Account_Management/forms.py
+++ Account_Management/forms.py
@@ -4,9 +4,10 @@ from .models import *
 
 
 class InviteForm(forms.Form):
-    email = forms.EmailField(required=True, label='Email')
-    role_name = forms.CharField(max_length=255)
-    role_type = forms.ChoiceField(choices=Role.ROLE_TYPE_CHOICES)
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'shadow-border'}), required=True, label='Email')
+    role_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'shadow-border'}), max_length=255)
+    role_type = forms.ChoiceField(widget=forms.Select(attrs={'class': 'choice-field'}),
+                                  choices=Role.ROLE_TYPE_CHOICES)
 
 
 class OrganizationForm(forms.Form): 
