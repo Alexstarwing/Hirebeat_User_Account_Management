8539affe22c31433e3d3b175623be4fb06ce07d5
Spriteson <duan_hui@hotmail.com>
2023-08-28 19:52:20 +0800

    added accout_user_relation in admin

--- Account_Management/admin.py
+++ Account_Management/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Account, Role, TeamInvitation
+from .models import Account, Role, TeamInvitation, AccountUserRelation
 
 # Register your models here.
 admin.site.register(Account)
 admin.site.register(Role)
 admin.site.register(TeamInvitation)
+admin.site.register(AccountUserRelation)
